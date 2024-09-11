180931a49abac128774f75374f1c543057cfce21
vaxirishabh <gdscrishabh@gmail.com>
2024-09-11 23:40:02 +0530

    Patern 4rt

---
+++ src/patern4Rt.java
@@ -0,0 +1,17 @@
+public class patern4Rt {
+
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+// inner loop → space print
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+//inner loop → star print
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+        }
+    }
+}
